bb0b199b9580889cb3838e3fee487729558bfcd8
Camilo Sanchez <camilin87@gmail.com>
2018-04-25 20:36:46 -0400

    enable EFCore debug logging

--- MyDbContext.cs
+++ MyDbContext.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DebugEFCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace DebugEFCoreSample
 {
   public class MyDbContext : DbContext
   {
+    public static readonly bool DEBUG_ENABLED = false; 
+    
     public DbSet<Employee> Employees { get; set; }
     
     public MyDbContext(DbContextOptions options) : base(options) { }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.EnableLogging(DEBUG_ENABLED);
+    }
   }
 }
