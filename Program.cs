f820ef90e6dd448245961cf21d428b5ea7850755
Camilo Sanchez <camilin87@gmail.com>
2018-04-25 20:29:59 -0400

    Create a new Employee in the Context whenever the application is executed

--- Program.cs
+++ Program.cs
@@ -1,24 +1,48 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Xml;
 using log4net;
 using log4net.Config;
 using log4net.Repository.Hierarchy;
+using Microsoft.EntityFrameworkCore;
 
 namespace DebugEFCoreSample
 {
   class Program
   {
     private static readonly ILog Logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
-    
+    private static readonly Random generator = new Random();
+
     static void Main(string[] args)
     {
       ConfigureLog4Net();
 
       Logger.Info("Started");
+
+      using (var context = new MyDbContextFactory().CreateDbContext())
+      {
+        context.Database.Migrate();
+
+        Logger.Info($"Employees Count Before: {context.Employees.Count()}");
+
+        context.Employees.Add(CreateEmployee());
+        context.SaveChanges();
+
+        Logger.Info($"Employees Count After: {context.Employees.Count()}");
+      }
+
       Logger.Info("Finished");
     }
+
+    private static Employee CreateEmployee() => new Employee
+    {
+      Age = generator.Next(20, 55),
+      FirstName = $"Fred{generator.Next(1000, 10000)}",
+      LastName = $"Doe{generator.Next(1000, 10000)}",
+      Title = $"Software Engineer {generator.Next(1, 3)}"
+    };
 
     private static void ConfigureLog4Net()
     {
